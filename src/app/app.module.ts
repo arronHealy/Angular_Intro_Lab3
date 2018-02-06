868524797925aeca732ac7857954f16bc89e0ba9
arronHealy <G00346883@gmit.ie>
2018-02-06 17:43:21 +0000

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ComponentaComponent } from './componenta/componenta.component';
+import { ComponentbComponent } from './componentb/componentb.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentaComponent,
+    ComponentbComponent
   ],
   imports: [
     BrowserModule
